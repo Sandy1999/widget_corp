85adda47ee5e91e6adb5c3ba74aa6f6ec4566b68
Sandy1999 <Sandyrockstar19@gmail.com>
2017-07-03 22:08:23 +0530

    Add files via upload

--- widget_corp.sql
+++ widget_corp.sql
@@ -1,108 +1,139 @@
--- MySQL dump 10.13  Distrib 5.5.42, for Win32 (x86)
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: widget_corp
--- ------------------------------------------------------
--- Server version	5.5.42
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2017 at 04:34 PM
+-- Server version: 5.5.42
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `widget_corp`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `admins`
 --
 
-DROP TABLE IF EXISTS `admins`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `admins` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admins`
 --
 
-LOCK TABLES `admins` WRITE;
-/*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-INSERT INTO `admins` VALUES (1,'sanidhya','$2y$10$ZTc5ZWVhYzVhMjc4ODllO.6hx2KVNygyu3YpLJLxc6N3QzjoHZ4qS'),(7,'sandy123','$2y$10$ODFlYzM5NWE0OWRjYTZmMOB1JaCbX3rI4TWgon3YkSbW977RsWhPe');
-/*!40000 ALTER TABLE `admins` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `admins` (`id`, `username`, `password`) VALUES
+(1, 'sanidhya', '$2y$10$ZTc5ZWVhYzVhMjc4ODllO.6hx2KVNygyu3YpLJLxc6N3QzjoHZ4qS'),
+(7, 'sandy123', '$2y$10$ODFlYzM5NWE0OWRjYTZmMOB1JaCbX3rI4TWgon3YkSbW977RsWhPe');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `pages`
 --
 
-DROP TABLE IF EXISTS `pages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `subject_id` int(11) NOT NULL,
   `menu_name` varchar(30) NOT NULL,
   `position` int(3) NOT NULL,
   `visible` tinyint(1) NOT NULL,
-  `content` text,
-  PRIMARY KEY (`id`),
-  KEY `subject_id` (`subject_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `content` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pages`
 --
 
-LOCK TABLES `pages` WRITE;
-/*!40000 ALTER TABLE `pages` DISABLE KEYS */;
-INSERT INTO `pages` VALUES (1,1,'My Mission',1,1,'My Mission is been to learn and implement PHP at its best'),(2,1,'Creation Time',2,1,'This is a basic CMS took around three days to built properly.'),(3,2,'PHP',1,1,'In this test CMS PHP was rigorously used and its back hand is also a PHP. '),(4,2,'MySQL',2,1,'Any project is incomplete without a database so to make it efficient and retro MySQL as data base is been used. '),(5,3,'CMS',1,1,'Its a content management system which works dynamically in all its way. Can be used to create and manage personal blogs.');
-/*!40000 ALTER TABLE `pages` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `pages` (`id`, `subject_id`, `menu_name`, `position`, `visible`, `content`) VALUES
+(1, 1, 'My Mission', 1, 1, 'My Mission is been to learn and implement PHP at its best'),
+(2, 1, 'Creation Time', 2, 1, 'This is a basic CMS took around three days to built properly.'),
+(3, 2, 'PHP', 1, 1, 'In this test CMS PHP was rigorously used and its back hand is also a PHP. '),
+(4, 2, 'MySQL', 2, 1, 'Any project is incomplete without a database so to make it efficient and retro MySQL as data base is been used. '),
+(5, 3, 'CMS', 1, 1, 'Its a content management system which works dynamically in all its way. Can be used to create and manage personal blogs.');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `subjects`
 --
 
-DROP TABLE IF EXISTS `subjects`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `subjects` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `menu_name` varchar(30) NOT NULL,
   `position` int(3) NOT NULL,
-  `visible` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `visible` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `subjects`
 --
 
-LOCK TABLES `subjects` WRITE;
-/*!40000 ALTER TABLE `subjects` DISABLE KEYS */;
-INSERT INTO `subjects` VALUES (1,'About Personal CMS',1,1),(2,'Inclusions',2,1),(3,'Services',3,1),(4,'Misc',4,0),(5,'Sample subject',5,0),(6,'',1,0);
-/*!40000 ALTER TABLE `subjects` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `subjects` (`id`, `menu_name`, `position`, `visible`) VALUES
+(1, 'About Personal CMS', 1, 1),
+(2, 'Inclusions', 2, 1),
+(3, 'Services', 3, 1),
+(4, 'Misc', 4, 0),
+(5, 'Sample subject', 5, 0),
+(6, '', 1, 0);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `subject_id` (`subject_id`);
+
+--
+-- Indexes for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `subjects`
+--
+ALTER TABLE `subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-06-30 17:20:43
